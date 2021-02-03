b73452e5ddbb6bfeabc52949fa4a5ae2d6ae1a38
Quang NV <quangnv1311@gmail.com>
2021-02-03 16:03:52 +0700

    add chat message

--- client/index.js
+++ client/index.js
@@ -8,4 +8,42 @@ const avatarProfileImg = document.getElementById('avatar-profile');
 const displayNameProfileInput = document.getElementById('display-name-input');
 const avatarProfileInput = document.getElementById('avatar-input');
 
-let chatMessages = [];
+socket.on('message', messages => {
+    renderChat(messages);
+});
+
+function renderChat(newMessages = []) {
+    let flag = document.createDocumentFragment();
+    newMessages.forEach(message => {
+        let wrapperDiv = document.createElement('div');
+        wrapperDiv.className += 'p-2 border mt-2 chat-message';
+
+        let avatarImg = document.createElement('img');
+        avatarImg.width = '50px';
+        avatarImg.src = message.avatar;
+        wrapperDiv.appendChild(avatarImg);
+
+        let displayNameSpan = document.createElement('span');
+        displayNameSpan.textContent = message.displayName + ': ';
+        wrapperDiv.appendChild(displayNameSpan);
+
+        let messageSpan = document.createElement('span');
+        messageSpan.textContent = message.message;
+        wrapperDiv.appendChild(messageSpan);
+
+        flag.appendChild(wrapperDiv);
+    });
+    chatMessagesDiv.appendChild(flag);
+}
+
+
+function makeChatId() {
+    var result = "";
+    var characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_)(:/?.,,[]-=+@!#";
+    var charactersLength = characters.length;
+    for (var i = 0; i < 15; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
--- server/app.js
+++ server/app.js
@@ -23,20 +23,22 @@ io.on("connection", (socket) => {
     console.log(`Client id: ${socket.id} is disconnected`);
   });
 
-  socket.on("get-old-messages", id => {
-      console.log(id);
+  socket.on("get-old-messages", (id) => {
+    console.log(id);
     const index = db.get("messages").findIndex(id);
-    io.to(socket.id).emit("old-messages", db.get("messages").slice(index - 50, index));
+    io.to(socket.id).emit(
+      "old-messages",
+      db.get("messages").slice(index - 50, index)
+    );
   });
 
   socket.on("chat", (message) => {
-      console.log(message);
+    console.log(message);
     if (!message.avatar) {
       message.avatar =
         "https://www.hostpapa.com/knowledgebase/wp-content/uploads/2018/04/1-13.png";
     }
-    db.get('messages')
-    .push(message).write();
+    db.get("messages").push(message).write();
     io.emit("new-message", message);
   });
 });
